69a40da3189a849964e5192133ba534c3b8337af
MaxKreischer <MKreischer@gmx.net>
2017-11-25 12:44:57 +0100

    gauss filter still crappy

--- assignment2/problem1.jl
+++ assignment2/problem1.jl
@@ -1,22 +1,60 @@
 using Images
 using PyPlot
 
-
+F2I(x) = convert(Int64, x);
 # Create a gaussian filter
 function makegaussianfilter(size::Array{Int,2},sigma::Float64)
-
+  gauss(x, sigma) = exp(-(x^2.0/(2*(sigma^2.0))));
+  nrows = size[1];
+  ncols = size[2];
+  filterX = zeros(nrows,1);
+  filterY = zeros(1,ncols);
+  if(nrows%2==0)
+    throw("NOT IMPLEMENTED YET! -> makegaussianfilter for even filters.")
+  else
+    # X direction
+    #liX = linspace(0,3*sigma,ceil(nrows/2))
+    #filterX_ = map(g, liX);
+    filterX_ = [(1.0/sqrt(2.0*pi)*sigma)*exp(-(x^2.0/(2*(sigma^2.0)))) for x in linspace(0, 3*sigma, ceil(nrows/2))];
+    halfX = convert(Int64, ceil(nrows/2));
+    filterX[1:halfX-1] = copy(filterX_[end:-1:2]);
+    filterX[halfX] = copy(filterX_[1]);
+    filterX[halfX+1:end] = copy(filterX_[2:end]);
+    # Y direction
+    #liY = linspace(0,3*sigma,ceil(ncols/2))
+    #filterY_ = map(g, liY);
+    filterY_ = [(1.0/sqrt(2.0*pi)*sigma)*exp(-(y^2.0/(2*(sigma^2.0)))) for y in linspace(0, 3*sigma, ceil(ncols/2))];
+    halfY = convert(Int64, ceil(ncols/2));
+    filterY[1:halfY-1] = copy(filterY_[end:-1:2]);
+    filterY[halfY] = copy(filterY_[1]);
+    filterY[halfY+1:end] = copy(filterY_[2:end]);
+  end
+  f = filterX * filterY;
+  #f = copy(f ./ sum(f));
   return f::Array{Float64,2}
 end
 
-# Create a binomial filter
+# Create a binomial filter1
 function makebinomialfilter(size::Array{Int,2})
-
+  n_pick_k(N,k) = factorial(N)/(factorial(k)*factorial(N-k));
+  nrows = size[1]-1;
+  ncols = size[2]-1;
+  # X
+  weightsX = [n_pick_k(nrows, i) for i=0:nrows];
+  # Y
+  weightsY = [n_pick_k(ncols, i) for i=0:ncols];
+  f = weightsX'' * weightsY';
+  f = f ./ sum(f);
   return f::Array{Float64,2}
 end
 
 # Downsample an image by a factor of 2
 function downsample2(A::Array{Float64,2})
-
+  nrows,ncols = size(A);
+  rowIdx = 1:2:nrows;
+  colIdx = 1:2:ncols;
+  D = [A[i,j] for i in rowIdx for j in colIdx];
+  D = reshape(D, (F2I(ceil(nrows/2)), F2I(ceil(ncols/2))))'
   return D::Array{Float64,2}
 end
 
@@ -26,7 +64,7 @@ function upsample2(A::Array{Float64,2},fsize::Array{Int,2})
   return U::Array{Float64,2}
 end
 
-# Build a gaussian pyramid from an image. 
+# Build a gaussian pyramid from an image.
 # The output array should contain the pyramid levels in decreasing sizes.
 function makegaussianpyramid(im::Array{Float32,2},nlevels::Int,fsize::Array{Int,2},sigma::Float64)
 
@@ -68,10 +106,13 @@ function problem1()
   nlevels = 6
 
   # load image
-  im = PyPlot.imread("../data/a2p1.png")
+  im = PyPlot.imread("./data-julia/a2p1.png")
 
   # create gaussian pyramid
   G = makegaussianpyramid(im,nlevels,fsize,sigma)
+
+  return "Test RET"
+
 
   # display gaussianpyramid
   displaypyramid(G)
