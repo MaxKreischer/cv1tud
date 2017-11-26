314ea81de1e889764110482b14434b03485b4346
Max Kreischer <MKreischer@gmx.net>
2017-11-26 19:49:14 +0100

    Done

--- assignment2/problem2.jl
+++ assignment2/problem2.jl
@@ -54,13 +54,44 @@ end
 
 # Compute required number of components to account for (at least) 80/95 % of the variance
 function computencomponents(cumvar::Array{Float64,1})
+  thresh_80 = (80/100)*cumvar[end];
+  thresh_95 = (95/100)*cumvar[end];
+  lam = zeros(size(cumvar)[1]);
+  for i=0:(size(cumvar)[1]-2)
+    lam[i+1] = cumvar[end-i]-cumvar[end-i-1];
+  end
+  lam[end] = cumvar[1];
+  cumlam = cumsum(lam);
+  bThresh80 = [cumlam[i]<=thresh_80 for i=1:size(cumlam)[1]];
+  n80 = countnz(bThresh80);
+  bThresh95 = [cumlam[i]<=thresh_95 for i=1:size(cumlam)[1]];
+  n95 = countnz(bThresh95);
 
   return n80::Int,n95::Int
 end
 
 # Display the mean face and the first 10 Eigenfaces in a single figure
 function showfaces(U::Array{Float64,2},mu::Array{Float64,2},facedim::Array{Int})
-
+  dim = (facedim[1],facedim[2]);
+  filler = zeros((size(mu)[1],size(mu)[2]*4));
+  mFace = hcat(mu,filler);
+  #imshow(mFace, "gray", interpolation="none")
+  eigenfaces1 = zeros(size(mu)[1],size(mu)[2]*5);
+  eigenfaces2=eigenfaces1;
+  for i=0:9
+    if i==0 || i==5
+      eigenfaces1[:,1:dim[2]] = reshape(U[:,1],dim);
+      eigenfaces2[:,1:dim[2]] = reshape(U[:,6],dim);
+    elseif i<=4
+      eigenfaces1[:,(i*dim[2]+1):((i+1)*dim[2])] = reshape(U[:,i+1],dim);
+    else
+      eigenfaces2[:,((i-5)*dim[2]+1):((i-5+1)*dim[2])] = reshape(U[:,i+1],dim);
+    end
+  end
+  A = vcat(mFace, eigenfaces1,eigenfaces2);
+  figure()
+  imshow(A,"gray", interpolation="none")
+  axis("off")
   return nothing::Void
 end
 
@@ -73,7 +104,14 @@ end
 # Project a given face into the low-dimensional space with a given number of principal
 # components and reconstruct it afterwards
 function computereconstruction(faceim::Array{Float64,2},U::Array{Float64,2},mu::Array{Float64,2},n::Int)
-
+  dim = (size(faceim)[1],size(faceim)[2]);
+  faceim_ser = reshape(faceim,dim[1]*dim[2]);
+  mu_ser = reshape(mu,dim[1]*dim[2]);
+  face_zMean = faceim_ser - mu_ser;
+  B = U[:,1:n];
+  face_proj = B'*face_zMean;
+  face_rec = mu_ser + B*face_proj;
+  recon = reshape(face_rec, dim);
   return recon::Array{Float64,2}
 end
 
